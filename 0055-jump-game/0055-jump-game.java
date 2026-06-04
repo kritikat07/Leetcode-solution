201f907264778663ec27f4645a935ddb923c730a
Kritika Tiwari <tiwarikritika178@gmail.com>
2026-06-04 11:15:48 +0530

    Time: 2 ms (87.99%), Space: 47.9 MB (60.7%) - LeetHub

---
+++ 0055-jump-game/0055-jump-game.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean canJump(int[] nums) {
+        // int reachable=0;
+        // for(int i=0;i<nums.length;i++){
+        //     if(i>reachable) return false;
+        //     reachable=Math.max(reachable,i+nums[i]);
+        // }
+        // return true;
+        int reach=0;
+        for(int i=0;i<nums.length;i++){
+            if(i>reach){
+                return false;
+            }
+            reach=Math.max(reach,i+nums[i]);
+        }
+        return true;
+        
+    }
+}
