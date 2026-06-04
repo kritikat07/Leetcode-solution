4b6ae624c06a97a916fdf19d5b07ed031d833e3f
Kritika Tiwari <tiwarikritika178@gmail.com>
2026-06-04 11:44:14 +0530

    Time: 1 ms (99.64%), Space: 47.4 MB (20.22%) - LeetHub

---
+++ 0045-jump-game-ii/0045-jump-game-ii.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int jump(int[] nums) {
+       int jump=0;
+       int far=0;
+       int end=0;
+       for(int i=0;i<nums.length-1;i++){
+          far=Math.max(far,i+nums[i]);
+          if(i==end){
+            jump++;
+            end=far;
+          }
+       }
+       return jump;
+    }
+}
