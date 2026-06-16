b54184af148f5e698783c64f028fed81640805c6
Kritika Tiwari <tiwarikritika178@gmail.com>
2026-06-16 20:43:05 +0530

    Time: 1 ms (99.95%), Space: 77.3 MB (47.77%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.java
+++ 0053-maximum-subarray/0053-maximum-subarray.java
@@ -11,15 +11,24 @@ class Solution {
         // }
         
         // return max;
-        int n=nums.length;
-        int max=Integer.MIN_VALUE;
-        int sum=0;
-        for(int num:nums){
-            sum+=num;
+    //     int n=nums.length;
+    //     int max=Integer.MIN_VALUE;
+    //     int sum=0;
+    //     for(int num:nums){
+    //         sum+=num;
             
-            max=Math.max(sum,max);
-            if(sum<0) sum=0;
-        }
-        return max;
+    //         max=Math.max(sum,max);
+    //         if(sum<0) sum=0;
+    //     }
+    //     return max;
+    int n=nums.length;
+    int sum=0;
+    int max=nums[0];
+    for(int num:nums){
+        sum+=num;
+        max=Math.max(sum,max);
+        if(sum<0) sum=0;
+    }
+    return max;
     }
 }
