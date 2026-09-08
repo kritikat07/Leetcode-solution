11873bcd4f89eed30bbf76e33c8045d29c787ed3
Kritika Tiwari <tiwarikritika178@gmail.com>
2026-09-08 23:31:55 +0530

    Time: 8 ms (8.47%), Space: 42.5 MB (57.08%) - LeetHub

---
+++ 3870-count-commas-in-range/3870-count-commas-in-range.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int countCommas(int n) {
+        int ans=0;
+        for(int i=0;i<=n;i++){
+            if(i>=1000){
+                ans++;
+            }
+        }
+        return ans;
+    }
+}
